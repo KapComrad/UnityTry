ae380226774eae396afeccf915a46eb8ec8c5b66
KapComrad <79967199+KapComrad@users.noreply.github.com>
2022-06-07 10:26:54 +0800

    Added foxy animation

---
+++ Assets/Script/PlayerAnimation.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Transform _transform;
+    private Rigidbody2D _rigidbody2D;
+    private Animator _animator;
+    private PlayerMovement _playerMovement;
+
+    [SerializeField] private bool _turningRight = true;
+
+    private void Start()
+    {
+        _animator = GetComponent<Animator>();
+        _transform = GetComponent<Transform>();
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _playerMovement = GetComponent<PlayerMovement>();
+    }
+    void Update()
+    {
+        if (!_turningRight && _rigidbody2D.velocity.x > 0.1f)
+        {
+            _transform.localScale = new Vector3(1f, 1f, 0f);
+            _turningRight = true;
+        }
+        else if (_turningRight && _rigidbody2D.velocity.x < -0.1f)
+        {
+            _transform.localScale = new Vector3(-1f, 1f, 0f);
+            _turningRight = false;
+        }
+        RunAnimationPlay();
+        JumpAnimationPlay();
+    }
+
+    private void RunAnimationPlay()
+    {
+        _animator.SetFloat("Speed", Mathf.Abs(_rigidbody2D.velocity.x));
+    }
+
+    private void JumpAnimationPlay()
+    {
+        _animator.SetFloat("JumpVelocity", _rigidbody2D.velocity.y);
+        _animator.SetBool("IsGrounded", _playerMovement.IsGrounded());
+    }
+}
--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,129 +2,134 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+namespace Player
 {
-    [Header("Movement")]
-    [SerializeField] private float _speed = 1f;
-    
-    [Header("Jump")]
-    [SerializeField] private float _jumpForce = 10f;
-    [SerializeField] private bool _isJumping = false;
-    [SerializeField] private Vector2 _groundCheckSize = new Vector2(0.2f, 0.2f);
-    [SerializeField] private float _lastGroundedTime = 0f;
-    [SerializeField] private float _jumpBufferTime = 0.2f;
-    [SerializeField] private float _jumpTime = 0f;
-    [SerializeField] private float _jumpMultiplier = 0f;
-    [SerializeField] private float _fallMultiplier = 0f;
-    [SerializeField] private float _jumpCounter = 0f;
-    private Vector2 _vecGravity;
-
-
-    [Header("GameObjects")]
-    [SerializeField] private Transform _groundCheckPoint;
-    private Rigidbody2D _rigidBody2D;
-
-    private LayerMask _groundLayer;
-    private float _InputHorizontal;
-
-    private void Start()
+    public class PlayerMovement : MonoBehaviour
     {
-        _vecGravity = new Vector2(0, -Physics2D.gravity.y);
-        _rigidBody2D = GetComponent<Rigidbody2D>();
-        _groundLayer = LayerMask.GetMask("Ground");
-    }
+        [Header("Movement")]
+        [SerializeField] private float _speed = 1f;
 
-    void Update()
-    {
-        CheckInput();
-        GravityMultiplier();
-        JumpMulpitlier();
-    }
-    private void FixedUpdate()
-    {
-        Movement();
-        CheckIsOnGround();
-    }
-    private void CheckInput()
-    {
-        _lastGroundedTime -= Time.deltaTime;
-        _InputHorizontal = Input.GetAxis("Horizontal");
-        
-        if (Input.GetButtonDown("Jump"))
-        { 
-            Jump();
-        }
-        if (Input.GetButtonUp("Jump"))
+        [Header("Jump")]
+        [SerializeField] private float _jumpForce = 10f;
+        public bool IsJumping { get; private set; }
+        [SerializeField] private Vector2 _groundCheckSize = new Vector2(0.2f, 0.2f);
+        [SerializeField] private float _lastGroundedTime = 0f;
+        [SerializeField] private float _jumpBufferTime = 0.2f;
+        [SerializeField] private float _jumpTime = 0f;
+        [SerializeField] private float _jumpMultiplier = 0f;
+        [SerializeField] private float _fallMultiplier = 0f;
+        [SerializeField] private float _jumpCounter = 0f;
+        private Vector2 _vecGravity;
+
+
+        [Header("GameObjects")]
+        [SerializeField] private Transform _groundCheckPoint;
+        private Rigidbody2D _rigidbody2D;
+
+        private LayerMask _groundLayer;
+        private float _InputHorizontal;
+
+        private void Start()
         {
-            _isJumping = false;
-            _jumpCounter = 0;
+            _vecGravity = new Vector2(0, -Physics2D.gravity.y);
+            _rigidbody2D = GetComponent<Rigidbody2D>();
+            _groundLayer = LayerMask.GetMask("Ground");
+        }
 
-            if (_rigidBody2D.velocity.y > 0 )
+        void Update()
+        {
+            CheckInput();
+            GravityMultiplier();
+            JumpMulpitlier();
+        }
+        private void FixedUpdate()
+        {
+            Movement();
+            CheckIsOnGround();
+        }
+        private void CheckInput()
+        {
+            _lastGroundedTime -= Time.deltaTime;
+            _InputHorizontal = Input.GetAxis("Horizontal");
+
+            if (Input.GetButtonDown("Jump"))
             {
-                _rigidBody2D.velocity = new Vector2(_rigidBody2D.velocity.x, _rigidBody2D.velocity.y * 0.6f);
+                Jump();
+            }
+            if (Input.GetButtonUp("Jump"))
+            {
+                IsJumping = false;
+                _jumpCounter = 0;
+
+                if (_rigidbody2D.velocity.y > 0)
+                {
+                    _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, _rigidbody2D.velocity.y * 0.6f);
+                }
+            }
+        }
+
+        private void CheckIsOnGround()
+        {
+            if (IsGrounded())
+            {
+                _lastGroundedTime = _jumpBufferTime;
+            }
+        }
+        public bool IsGrounded()
+        {
+            return Physics2D.OverlapBox(_groundCheckPoint.position, _groundCheckSize, 0, _groundLayer);
+        }
+
+        private void Movement()
+        {
+            _rigidbody2D.velocity = new Vector2(_speed * _InputHorizontal, _rigidbody2D.velocity.y);
+        }
+
+        /*
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireCube(_groundCheckPoint.position, _groundCheckSize);
+        }
+        */
+
+        private void Jump()
+        {
+            if (_lastGroundedTime > 0 && IsGrounded())
+            {
+                _lastGroundedTime = 0f;
+                IsJumping = true;
+                _jumpCounter = 0;
+                _rigidbody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
+            }
+        }
+
+        private void GravityMultiplier()
+        {
+            if (_rigidbody2D.velocity.y < 0.1f)
+            {
+                _rigidbody2D.velocity -= _vecGravity * _fallMultiplier * Time.deltaTime;
+            }
+        }
+
+        private void JumpMulpitlier()
+        {
+            if (_rigidbody2D.velocity.y > 0 && IsJumping)
+            {
+                _jumpCounter += Time.deltaTime;
+                if (_jumpCounter > _jumpTime) IsJumping = false;
+
+                float t = _jumpCounter / _jumpTime;
+                float currentJumpM = _jumpMultiplier;
+
+                if (t > 0.5f)
+                {
+                    currentJumpM = _jumpMultiplier * (1 - t);
+                }
+
+                _rigidbody2D.velocity += _vecGravity * currentJumpM * Time.deltaTime;
             }
         }
     }
 
-    private void CheckIsOnGround()
-    {
-        if (IsGrounded())
-        {
-            _lastGroundedTime = _jumpBufferTime;
-        }
-    }
-    private bool IsGrounded()
-    {
-        return Physics2D.OverlapBox(_groundCheckPoint.position, _groundCheckSize, 0, _groundLayer);
-    }
 
-    private void Movement()
-    {
-        _rigidBody2D.velocity = new Vector2(_speed * _InputHorizontal, _rigidBody2D.velocity.y);
-    }
-
-    /*
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireCube(_groundCheckPoint.position, _groundCheckSize);
-    }
-    */
-
-    private void Jump()
-    {
-        if (_lastGroundedTime > 0 && IsGrounded())
-        {
-            _lastGroundedTime = 0f;
-            _isJumping = true;
-            _jumpCounter = 0;
-            _rigidBody2D.AddForce(Vector2.up * _jumpForce, ForceMode2D.Impulse);
-        }
-    }
-
-    private void GravityMultiplier()
-    {
-        if (_rigidBody2D.velocity.y < 0.1f)
-        {
-            _rigidBody2D.velocity -= _vecGravity * _fallMultiplier * Time.deltaTime;
-        }
-    }
-
-    private void JumpMulpitlier()
-    {
-        if (_rigidBody2D.velocity.y > 0 && _isJumping)
-        {
-            _jumpCounter += Time.deltaTime;
-            if (_jumpCounter > _jumpTime) _isJumping = false;
-
-            float t = _jumpCounter / _jumpTime;
-            float currentJumpM = _jumpMultiplier;
-
-            if (t > 0.5f)
-            {
-                currentJumpM = _jumpMultiplier * (1 - t);
-            }
-
-            _rigidBody2D.velocity += _vecGravity * currentJumpM * Time.deltaTime;
-        }
-    }
 }
