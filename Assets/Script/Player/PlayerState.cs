9df053a88db2b00b8afd9f7d9e21add888d50d1b
KapComrad <79967199+KapComrad@users.noreply.github.com>
2022-06-09 12:34:15 +0800

    Sorting folders

--- Assets/Script/Player/PlayerState.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-public abstract class PlayerState
-{
-    public abstract void EnterState(PlayerStateMachine player);
-    public abstract void UpdateState(PlayerStateMachine player);
-}
